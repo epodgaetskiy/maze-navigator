75e0dd7fabedea3764fbaafd1aff275e1ef004be
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-05-11 21:37:19 +0300

    Updates

---
+++ precache-manifest.fddbe517ff612c550508a3d1bf1fecc0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8556762c5e5c027a56cd",
+    "url": "/maze-navigator/static/js/runtime~main.ecfa6432.js"
+  },
+  {
+    "revision": "c041a61dbcd947ad2a38",
+    "url": "/maze-navigator/static/js/main.4bcc00a3.chunk.js"
+  },
+  {
+    "revision": "8b11e5ed905505c05886",
+    "url": "/maze-navigator/static/js/2.12c00691.chunk.js"
+  },
+  {
+    "revision": "78ffb6c4129fd9dff5f97b6db5ce3c2a",
+    "url": "/maze-navigator/index.html"
+  }
+];
